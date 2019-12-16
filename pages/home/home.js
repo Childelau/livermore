0acdced7afb415d3a2f2ff198066e13629dbb47f
livermore <1299337429@qq.com>
2019-12-16 16:53:12 +0800

    增加资料

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,3 +1,25 @@
 Page({
-  
+    data:{
+        state: 0,
+        _num:'0'
+    },
+    // 点击tab切换
+    toggle(e){
+        if (this.data._num == e.currentTarget.dataset.index) {
+            return false
+        } else {
+            this.setData({
+                _num: e.currentTarget.dataset.index
+            })
+        }
+
+
+    },
+    bindchange:function(e){
+        console.log(e.detail.current)
+          
+    }
+
+
 })
+
