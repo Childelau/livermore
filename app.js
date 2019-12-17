fdd9d5833698b2311bcf8f84ddd0ec302bec3932
Childelau <1299337429@qq.com>
2019-12-17 23:46:48 +0800

    cloud,wxui

--- app.js
+++ app.js
@@ -1,23 +1,6 @@
 //app.js
 App({
   onLaunch: function () {
-    wx.cloud.init()
-    const db = wx.cloud.database()
-    db.collection('test').get({
-        success:function(res){
-            console.log(res.data)
-        }
-    })
-
-
-
-
-
-
-
-
-
-
 
 
 
--- pages/home/home.js
+++ pages/home/home.js
@@ -1,5 +1,17 @@
 Page({
+  onLoad:function(){
+    wx.cloud.init()
+    const db = wx.cloud.database()
+    db.collection('test').get({
+        success:function(res){
+            console.log(res)
+            console.log(res.data)
+        }
+    })
 
+
+    
+  }
 
 
 
