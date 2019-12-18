b008c988f3521763c062e5b916c1119de09c3ab4
livermore <1299337429@qq.com>
2019-12-18 16:49:09 +0800

    csv导入

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,18 +1,26 @@
 Page({
+  // data: {
+  //   Array: null
+  // },
+ 
   onLoad:function(){
+    var that = this
     wx.cloud.init()
     const db = wx.cloud.database()
-    db.collection('test').get({
+    db.collection('homeList').get({
         success:function(res){
             console.log(res)
             console.log(res.data)
+            that.setData({
+              Array:res.data
+            }) 
+ 
         }
     })
 
-
+  
     
   }
-
 
 
 
